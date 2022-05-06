7564f91d85ef0aea78801926baa46b5a033f6634
Kshitijv24 <kshitijv24@gmail.com>
2022-05-06 08:28:55 +0530

    Added Project To GitHub

---
+++ Protect Your Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int timeToWait;
+    
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine("WaitForTime");
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
