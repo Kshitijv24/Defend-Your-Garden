c41da7e4b8aa606914ebd49aa61fa5809bb76869
Kshitijv24 <kshitijv24@gmail.com>
2022-06-23 12:06:19 +0530

    added Projectile Movement in the game of a defender Plant

---
+++ Protect Your Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
---
+++ Protect Your Garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject gun;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
