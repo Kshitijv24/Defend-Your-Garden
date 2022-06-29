78f629d0646e78079f18206b0779908b63dd581e
Kshitijv24 <kshitijv24@gmail.com>
2022-06-29 20:52:34 +0530

    added stars that our sun flower generate over time which gives us points to place the defenders

--- Protect Your Garden/Assets/Scripts/Defender.cs
+++ Protect Your Garden/Assets/Scripts/Defender.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int starCost = 100;
+
+    public void AddStarsCaller(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
 }
