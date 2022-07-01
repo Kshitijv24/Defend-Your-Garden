c1a5fb899cd9c3c6ae1a72b1bd724ac633db3540
Kshitijv24 <kshitijv24@gmail.com>
2022-07-01 18:34:29 +0530

    Fixed Some Animations that were creating problem in the code

--- Protect Your Garden/Assets/Scripts/Shooter.cs
+++ Protect Your Garden/Assets/Scripts/Shooter.cs
@@ -5,6 +5,52 @@ public class Shooter : MonoBehaviour
     [SerializeField] private GameObject projectile;
     [SerializeField] private GameObject gun;
 
+    private AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot pew pew!");
+        }
+        else
+        {
+            Debug.Log("Sit and wait");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
