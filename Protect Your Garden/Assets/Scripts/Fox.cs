91a4b954e521107fa65dbc173ad812634be05f85
Kshitijv24 <kshitijv24@gmail.com>
2022-07-07 12:33:08 +0530

    added a new Attacker called Fox which can jump over our gravestone defender

---
+++ Protect Your Garden/Assets/Scripts/Fox.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
---
+++ Protect Your Garden/Assets/Scripts/Gravestone.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Attacker attacker = otherCollider.GetComponent<Attacker>();
+    }
+}
